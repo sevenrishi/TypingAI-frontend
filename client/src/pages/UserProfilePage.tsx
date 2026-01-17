4a60c48f92a5c6ae88dfaa469b9b7c193d6b42ba
Saptarshi Mondal <saptarshimondal768@gmail.com>
2026-01-18 01:21:20 +0530

    Profile page close button functionality fix

--- client/src/pages/UserProfilePage.tsx
+++ client/src/pages/UserProfilePage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import ProfilePage from '../features/user/components/ProfilePage';
 
 export default function UserProfilePage() {
+  const navigate = useNavigate();
+
   return (
     <div className="space-y-8">
-      <ProfilePage onClose={() => {}} />
+      <ProfilePage onClose={() => navigate('/')} />
     </div>
   );
 }
