5c9e5fbe2287701d04ea4616027d800fbd036f27
Saptarshi Mondal <saptarshimondal768@gmail.com>
2026-01-25 00:30:16 +0530

    Remove backend from monorepo

--- server/src/routes/aiRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { generate } from '../controllers/aiController';
-
-const router = Router();
-
-router.post('/generate', generate);
-
-export default router;
